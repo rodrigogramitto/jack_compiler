00e93061ace77d702f4540cf349cb98ea2318ec9
rodrigogramitto <rgramitto@gmail.com>
2026-04-18 20:57:26 -0700

    Ready to start development

--- src/syntax_analyzer/jack_analyzer.py
+++ src/syntax_analyzer/jack_analyzer.py
@@ -23,10 +23,3 @@ class JackAnalyzer:
     # needs to make an output dir too
     for file_path in directory.iterdir():
       self.compile_file(file_path)
-
-  # Main module, for each file in the input it:
-      # 1. creates a jackTokenizer from fileName.jack
-      # 2. creates an output file named fileName.xml
-      # 3. Creates a compilation engine, and calls the compileClass method
-      #     (compileClass wil then do the rest of the parsing, recursively)
-      # 4. Closes the output file.
