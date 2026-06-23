bfd6ab05bc383d485b1c7f593b9f06efa8942300
rodrigogramitto <rgramitto@gmail.com>
2026-06-23 15:02:36 -0700

    Create boilerplate VMWriter module

---
+++ src/code_generator/VMWriter.py
@@ -0,0 +1,35 @@
+# Generates VM Code
+
+class VMWriter:
+  def __init__(self):
+    return
+
+  def write_push(self, segment, index):
+    return
+
+  def write_pop(self, segment, index):
+    return
+
+  def write_arithmetic(self, command):
+    return
+
+  def write_label(self, label):
+    return
+
+  def write_goto(self, label):
+    return
+
+  def write_if(self, label):
+    return
+
+  def write_call(self, name, nArgs):
+    return
+
+  def write_function(self, name, nVars):
+    return
+
+  def write_return(self):
+    return
+
+  def close(self):
+    return
